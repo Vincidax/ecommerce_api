1b7ba69db905e3f566afcde0748a7cb87ab957b3
vincidax <vincentdx18@gmail.com>
2026-01-03 22:33:05 +0200

    revising serializer.py

--- products/serializers.py
+++ products/serializers.py
@@ -1,20 +1,29 @@
 from rest_framework import serializers
 from .models import Product, Category
 
+# Category Serializer
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
 
 
+# Product Serializer
 class ProductSerializer(serializers.ModelSerializer):
-    category_id = serializers.IntegerField(write_only=True)
-    category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)  # For creating/updating
+    category = CategorySerializer(read_only=True)            # Nested read
 
     class Meta:
         model = Product
         fields = "__all__"
 
     def create(self, validated_data):
-        category = Category.objects.get(id=validated_data.pop("category_id"))
+        # Get category safely
+        category_id = validated_data.pop("category_id")
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            raise serializers.ValidationError({"category_id": "Category does not exist."})
+        
+        # Create product
         return Product.objects.create(category=category, **validated_data)
