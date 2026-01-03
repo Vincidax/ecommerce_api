88f43aa63e42e48a9a7e6a908a33ed4231737a73
vincidax <vincentdx18@gmail.com>
2026-01-03 22:48:15 +0200

    add home endpoint

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from products.views import ProductViewSet, CategoryViewSet
 from users.views import UserViewSet
+from products.views import home
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
@@ -10,6 +11,7 @@ router.register('categories', CategoryViewSet)
 router.register('users', UserViewSet)
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,22 @@ from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.urls import reverse
+
+@api_view(['GET'])
+def home(request):
+    """
+    API Home - directs users to main endpoints
+    """
+    data = {
+        "products": request.build_absolute_uri(reverse('product-list')),
+        "categories": request.build_absolute_uri(reverse('category-list')),
+        "users": request.build_absolute_uri(reverse('user-list')),
+        "message": "Welcome to the E-commerce API! Use these endpoints to explore."
+    }
+    return Response(data)
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
